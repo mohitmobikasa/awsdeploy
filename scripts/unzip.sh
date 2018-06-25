b7ba2f888e5ba9b463d0dde3f80afdcaa77afbed
mohitaws <prishaaws@gmail.com>
2018-06-26 04:45:36 +0530

    restart httpd

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-cd /etc/httpd/conf.d/
-echo -e '<virtualhost *:80>\nDocumentroot /var/www/html/pmtool/public\n</virtualhost>\n'> virtualhost.conf
+service httpd restart
+#cd /etc/httpd/conf.d/
+#echo -e '<virtualhost *:80>\nDocumentroot /var/www/html/pmtool/public\n</virtualhost>\n'> virtualhost.conf
